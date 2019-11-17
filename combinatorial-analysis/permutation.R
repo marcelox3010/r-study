116696decefa469c6bd2e9d95802967fb5cd0f8e
Marcelo G Gadelha <marcelox3010@gmail.com>
2019-11-17 11:42:28 -0300

    Permutation simple, repeated elements

---
+++ combinatorial-analysis/permutation.R
@@ -0,0 +1,20 @@
+# PERMUTATION OF ABC
+# PERMUTAÇÃO DE ABC
+
+# ABC
+# ACB
+# BAC
+# BCA
+# CAB
+# CBA
+
+# 3!
+
+factorial(3)
+
+# PERMUTATION OF MISSISSIPPI
+# PERMUTAÇÃO DE MISSISSIPPI
+
+# 11! / (4!*4!*2!)
+
+factorial(11) / (factorial(4) * factorial(4) * factorial(2))
