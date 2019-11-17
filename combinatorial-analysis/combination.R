4d88531cbb07d34d5ac0081218f0cb7d569fbb30
Marcelo G Gadelha <marcelox3010@gmail.com>
2019-11-17 14:39:39 -0300

    Example of function of combination.

---
+++ combinatorial-analysis/combination.R
@@ -0,0 +1,14 @@
+
+# SIMPLE COMBINATION FUNCTION
+# FUNÇÃO DE COMBINAÇÃO SIMPLES
+
+# ( 5 )
+# |   |
+# ( 3 )
+
+n = 5
+k = 3
+
+choose(n, k) # 10
+
+factorial(n) / (factorial(k) * (factorial(n - k))) # 10
