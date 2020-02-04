a3267a9b3dce4c48f72b15f9f7bcf51e9086cbbe
marcelo.gadelha <marcelo.gadelha@cidadania.gov.br>
2020-02-04 14:23:42 -0300

    regressão com vários valores.

--- regressao-linear/exemplo.R
+++ regressao-linear/exemplo.R
@@ -1,11 +1,22 @@
 
 
+#https://www.datacamp.com/community/tutorials/linear-regression-R
+
 library(readxl);
 
 ageandheight <- read_excel("./regressao-linear/ageandheight.xls", sheet="Hoja2");
 
 plot(ageandheight[,1:2])
 
+#duas variáveis
 lmHeight <- lm(height~age, data=ageandheight);
 summary(lmHeight);
+
+#mais de duas variáveis
+lmHeight2 = lm(height~age + no_siblings, data = ageandheight);
+summary(lmHeight2);
+
+#todas as variáveis
+lmHeight3 = lm(height~., data = ageandheight);
+summary(lmHeight3);
 
