e731e351c1157b23c853e7ba75d780d7cf58ce31
marcelo.gadelha <marcelo.gadelha@cidadania.gov.br>
2020-05-09 17:37:34 -0300

    Livro: Estatítica aplicada às ciências sociais

---
+++ Barbetta-1994/Capitulo6.R
@@ -0,0 +1,32 @@
+#DADOS
+turma_a <- c(4,5,5,6,6,7,7,8)
+turma_b <- c(1,2,4,6,6,9,10,10)
+turma_c <- c(0,6,7,7,7,7.5,7.5)
+turma_d <- c(7,7,7,7,7,7,7)
+
+sd(turma_a)
+# FÓRMULA 1
+sqrt((sum((turma_a - mean(turma_a))^2)) / (length(turma_a) - 1))
+#FÓRMULA 2
+sqrt(((sum(turma_a^2)) - (length(turma_a) * (mean(turma_a)^2))) / (length(turma_a) - 1))
+#FÓRMULA 3
+turma_a_unicos <- unique(turma_a)
+turma_a_unicos_freq <- as.vector(table(turma_a))
+sqrt(((sum(turma_a_unicos^2 * turma_a_unicos_freq)) - (length(turma_a_unicos) * (mean(turma_a_unicos)^2))) / (length(turma_a_unicos) - 1))
+
+#Questão 1
+
+sd(turma_b)
+sd(turma_c)
+
+#Questão 2
+
+mean(turma_d)
+sd(turma_d)
+
+#Questão 3
+
+sd(turma_a) # FÓRMULA 1
+sqrt(((sum(turma_a^2)) - (length(turma_a) * (mean(turma_a)^2))) / (length(turma_a) - 1)) #FÓRMULA 2
+
+
