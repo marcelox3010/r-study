718ec9824316167ad98ae9b9d734805e61f815f1
marcelo.gadelha <marcelo.gadelha@cidadania.gov.br>
2020-05-14 16:10:51 -0300

    amostra

---
+++ dplyr/dplyr-sample_frac.R
@@ -0,0 +1,11 @@
+#
+# DPLYR SELECT
+#
+#install.packages("dplyr")
+
+library(dplyr)
+
+names(iris)
+dim(iris)
+sample_frac(iris, 0.1) # 15 porque é 10% de 150 (quantidade de observações)
+sample_n(iris, 10) # 10 amostras
