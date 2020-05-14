fb9d79ca25d74766f23800a8b7dfe883ed96f78c
Marcelo Gadelha <marcelox3010@gmail.com>
2020-05-13 22:35:40 -0300

    exemplo de sapply

--- applys/sapply.R
+++ applys/sapply.R
@@ -1,13 +1,14 @@
 #
-# EXEMPLO DE LAPPLY
+# EXEMPLO DE SAPPLY
 #
 
 movies        <- c("SPYDERMAN","BATMAN","VERTIGO","CHINATOWN")
-movies_lower  <- lapply(movies, tolower)
+movies_lower  <- sapply(movies, tolower)
 str(movies_lower)
 
 a <- list(c(1,2,3), matrix(1:10, nrow=2, ncol=5), data.frame(c(1,2,3), c(4,5,6), c(7,8,9)))
-lapply(a, sum)
+sapply(a, sum)
+
 
 dt <- cars
 lmn_cars <- lapply(dt, min)
