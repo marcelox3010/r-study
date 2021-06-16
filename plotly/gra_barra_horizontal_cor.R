66ba53d93f656d0bfeeb2efe0a790312d9b0d02c
marcelox3010 <marcelox3010@gmail.com>
2021-06-15 22:23:37 -0300

    Update gra_barra_horizontal_cor.R

--- plotly/gra_barra_horizontal_cor.R
+++ plotly/gra_barra_horizontal_cor.R
@@ -1,3 +1,4 @@
+#gráfico 1
 library(plotly)
 
 legendas <-  c('05/2021', 'Meta 2021')
@@ -16,4 +17,30 @@ fig <- fig %>% layout(title = titulo,
                       xaxis = list(title = "%"),
                       yaxis = list(title = ""))
 
-fi
+fig
+
+#gráfico 2
+
+titulo <- 'Famílias habilitadas ao PBF por situação (09/2020)'
+cores <- c("rgb(247,199,48)", "rgb(68,114,196)")
+
+saldo_famias_habilitadas <- c(95)
+novas_concessoes <- c(5)
+y <- c(as.character(saldo_famias_habilitadas+novas_concessoes))
+
+data <- data.frame(y, saldo_famias_habilitadas, novas_concessoes)
+
+fig <- plot_ly(data, x = ~saldo_famias_habilitadas, y = ~y, type = 'bar', orientation = 'h', name = 'Saldo Famílias Habilitadas',
+               marker = list(color = cores[1]))
+
+fig <- fig %>% add_trace(x = ~novas_concessoes, name = 'Novas Concessões',
+                         marker = list(color = cores[2]))
+fig <- fig %>% layout(barmode = 'stack',
+                      xaxis = list(title = ""),
+                      yaxis = list(title =""))
+
+fig <- fig %>% layout(title = titulo)
+fig <- fig %>% layout(legend = list(orientation = 'h'))
+
+fig
+
