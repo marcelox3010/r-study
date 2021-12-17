7629807d8f7d80f2fbe278f336262ccead7cd3e8
Marcelo Gadelham <marcelox3010@gmail.com>
2021-12-17 09:13:33 -0300

    buble sort: versão não finalizada.

--- algorirmos/bublle-sort.R
+++ algorirmos/bublle-sort.R
@@ -1,11 +1,36 @@
-#QUANTIDADE DE NÚMERO PARA ORDENAR
-n <- 100
-#numero inicial
+#QUANTIDADE DE NÚMEROS PARA ORDENAR
+qtd_elementos <- 100
+
+#NÚMERO INICIAL PARA AMOSTRA
 n1 <- 1
-#numero final
+
+#NÚMERO FINAL PARA AMOSTRA
 n2 <- 1000000
-#cria vetor de números aleatórios
-v <- sample(n1:n2, n, replace=TRUE)
 
-#buble sort
+#CRIA VETOR DE NÚMEROS ALEATÓRIOS
+vetor_numeros_para_ordenacao <- sample(n1:n2, qtd_elementos, replace=TRUE)
 
+#BUBLE SORT
+desordenado <- TRUE
+
+while(desordenado){
+  
+  desordenado <- FALSE
+  
+  for(i in 1:length(vetor_numeros_para_ordenacao)-1){
+    
+    if(vetor_numeros_para_ordenacao[i] > vetor_numeros_para_ordenacao[i+1]){
+      
+      print(i)
+      tmp <- vetor_numeros_para_ordenacao[i+1]
+      vetor_numeros_para_ordenacao[i] <- vetor_numeros_para_ordenacao[i+1]
+      vetor_numeros_para_ordenacao[i+1] <- tmp
+      desordenado <- TRUE
+      
+    }
+    
+  }
+  
+}
+
+v
