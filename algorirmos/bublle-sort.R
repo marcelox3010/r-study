6b274e3bcba7f7f28e150ce9eaa5791cf74de312
Marcelo Gadelham <marcelox3010@gmail.com>
2021-12-17 13:25:59 -0300

    BUBLE SORT: PRIMEIRA VERSÃO.

--- algorirmos/bublle-sort.R
+++ algorirmos/bublle-sort.R
@@ -1,36 +1,55 @@
-#QUANTIDADE DE NÚMEROS PARA ORDENAR
-qtd_elementos <- 100
-
-#NÚMERO INICIAL PARA AMOSTRA
-n1 <- 1
-
-#NÚMERO FINAL PARA AMOSTRA
-n2 <- 1000000
-
-#CRIA VETOR DE NÚMEROS ALEATÓRIOS
-vetor_numeros_para_ordenacao <- sample(n1:n2, qtd_elementos, replace=TRUE)
-
-#BUBLE SORT
-desordenado <- TRUE
-
-while(desordenado){
+#BUBLE SORT FUNCTION
+bubble_sort_gadelha <- function(vetor, ordem="ASC"){
   
-  desordenado <- FALSE
+  desordenado <- TRUE
   
-  for(i in 1:length(vetor_numeros_para_ordenacao)-1){
+  while(desordenado){
     
-    if(vetor_numeros_para_ordenacao[i] > vetor_numeros_para_ordenacao[i+1]){
+    desordenado <- FALSE
+    
+    for(i in 1:(length(vetor)-1)){
       
-      print(i)
-      tmp <- vetor_numeros_para_ordenacao[i+1]
-      vetor_numeros_para_ordenacao[i] <- vetor_numeros_para_ordenacao[i+1]
-      vetor_numeros_para_ordenacao[i+1] <- tmp
-      desordenado <- TRUE
+      # CASO A FUNÇÃO TENHA DESC NO PARÂMETRO ORDEM 
+      if(ordem=="DESC") p_ordem <- vetor[i] < vetor[i+1]
+      else p_ordem <- vetor[i] > vetor[i+1]
+      
+      if(p_ordem){
+        
+        # SE ELE ENTRAR NESSE ALGUMA VEZ SIGNIFICA QUE O VETOR ESTÁ DESORDENADO
+        desordenado <- TRUE
+        
+        # TROCA ELEMENTOS DE POSIÇÃO
+        tmp <- vetor[i+1]
+        vetor[i+1] <- vetor[i]
+        vetor[i] <- tmp
+        
+      }
       
     }
     
   }
   
+  return(vetor)
+  
 }
 
-v
+#QUANTIDADE DE NÚMEROS PARA ORDENAR
+qtd_elementos <- 100
+
+#NÚMERO INICIAL PARA AMOSTRA
+numero_inicial_para_geracao_numeros <- 1
+
+#NÚMERO FINAL PARA AMOSTRA
+numero_final_para_geracao_numeros <- 1000
+
+#CRIA VETOR DE NÚMEROS ALEATÓRIOS
+vetor_numeros_para_ordenacao <- 
+        
+  sample(
+    numero_inicial_para_geracao_numeros:numero_final_para_geracao_numeros, 
+    qtd_elementos, 
+    replace=TRUE
+  )
+
+bubble_sort_gadelha(vetor_numeros_para_ordenacao)
+bubble_sort_gadelha(vetor_numeros_para_ordenacao, "DESC")
