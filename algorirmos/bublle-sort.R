2e41f75a08c069d4d29bec5a9f74819100a36bf7
Marcelo Gadelham <marcelox3010@gmail.com>
2021-12-17 09:06:06 -0300

    buble sort: versão não finalizada.

---
+++ algorirmos/bublle-sort.R
@@ -0,0 +1,11 @@
+#QUANTIDADE DE NÚMERO PARA ORDENAR
+n <- 100
+#numero inicial
+n1 <- 1
+#numero final
+n2 <- 1000000
+#cria vetor de números aleatórios
+v <- sample(n1:n2, n, replace=TRUE)
+
+#buble sort
+
