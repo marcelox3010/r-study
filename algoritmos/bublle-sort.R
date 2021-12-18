61e727d2559bd31fab0b4effc58080d8fdf6cb22
Marcelo Gadelham <marcelox3010@gmail.com>
2021-12-17 21:45:27 -0300

    bubble sort: algoritmo atualizado.

--- algoritmos/bublle-sort.R
+++ algoritmos/bublle-sort.R
@@ -1,24 +1,15 @@
 #BUBLE SORT FUNCTION
-mgg_bubble_sort <- function(vetor, ordem="ASC"){
+mgg_bubble_sort <- function(vetor){
   
   for(minimo in 2:length(vetor)){
     
-    for(i in length(vetor):minimo){
+    for(indice in length(vetor):minimo){
       
-      # CASO A FUNÇÃO TENHA DESC NO PARÂMETRO ORDEM 
-      if(ordem=="DESC") p_ordem <- vetor[i] < vetor[i-1]
-      else p_ordem <- vetor[i] > vetor[i-1]
-      
-      if(p_ordem){
+      if(vetor[indice] < vetor[indice-1]){
         
-        # SE ELE ENTRAR NESTA CONDICIONAL ALGUMA VEZ 
-        # SIGNIFICA QUE O VETOR ESTÁ DESORDENADO
-        desordenado <- TRUE
-        
-        # TROCA ELEMENTOS DE POSIÇÃO
-        tmp <- vetor[i-1]
-        vetor[i-1] <- vetor[i]
-        vetor[i] <- tmp
+        tmp <- vetor[indice-1]
+        vetor[indice-1] <- vetor[indice]
+        vetor[indice] <- tmp
         
       }
       
@@ -26,18 +17,18 @@ mgg_bubble_sort <- function(vetor, ordem="ASC"){
     
   }
   
-  return(vetor)
+  vetor
   
 }
 
 #QUANTIDADE DE NÚMEROS PARA ORDENAR
-qtd_elementos <- 1000
+qtd_elementos <- 10
 
 #NÚMERO INICIAL PARA AMOSTRA
 numero_inicial_para_geracao_numeros <- 1
 
 #NÚMERO FINAL PARA AMOSTRA
-numero_final_para_geracao_numeros <- 1000
+numero_final_para_geracao_numeros <- 10
 
 #CRIA VETOR DE NÚMEROS ALEATÓRIOS
 vetor_numeros_para_ordenacao <- 
@@ -50,8 +41,3 @@ vetor_numeros_para_ordenacao <-
 
 vetor_numeros_para_ordenacao
 mgg_bubble_sort(vetor_numeros_para_ordenacao)
-mgg_bubble_sort(vetor_numeros_para_ordenacao, "DESC")
-
-vetor_numeros_para_ordenacao
-mgg_bubble_sort_2(vetor_numeros_para_ordenacao)
-mgg_bubble_sort_2(vetor_numeros_para_ordenacao, "DESC")
