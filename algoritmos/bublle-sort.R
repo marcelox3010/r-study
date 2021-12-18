3f7c83c4c2624c6e861e2f57d4b28ef4edb760a6
Marcelo Gadelham <marcelox3010@gmail.com>
2021-12-17 21:32:00 -0300

    bubble sort: correção de algoritmo. Evita passar pelos que já foram ordenador.

--- algoritmos/bublle-sort.R
+++ algoritmos/bublle-sort.R
@@ -1,17 +1,13 @@
 #BUBLE SORT FUNCTION
-bubble_sort_gadelha <- function(vetor, ordem="ASC"){
+mgg_bubble_sort <- function(vetor, ordem="ASC"){
   
-  desordenado <- TRUE
-  
-  while(desordenado){
+  for(minimo in 2:length(vetor)){
     
-    desordenado <- FALSE
-    
-    for(i in 1:(length(vetor)-1)){
+    for(i in length(vetor):minimo){
       
       # CASO A FUNÇÃO TENHA DESC NO PARÂMETRO ORDEM 
-      if(ordem=="DESC") p_ordem <- vetor[i] < vetor[i+1]
-      else p_ordem <- vetor[i] > vetor[i+1]
+      if(ordem=="DESC") p_ordem <- vetor[i] < vetor[i-1]
+      else p_ordem <- vetor[i] > vetor[i-1]
       
       if(p_ordem){
         
@@ -20,8 +16,8 @@ bubble_sort_gadelha <- function(vetor, ordem="ASC"){
         desordenado <- TRUE
         
         # TROCA ELEMENTOS DE POSIÇÃO
-        tmp <- vetor[i+1]
-        vetor[i+1] <- vetor[i]
+        tmp <- vetor[i-1]
+        vetor[i-1] <- vetor[i]
         vetor[i] <- tmp
         
       }
@@ -35,7 +31,7 @@ bubble_sort_gadelha <- function(vetor, ordem="ASC"){
 }
 
 #QUANTIDADE DE NÚMEROS PARA ORDENAR
-qtd_elementos <- 100
+qtd_elementos <- 1000
 
 #NÚMERO INICIAL PARA AMOSTRA
 numero_inicial_para_geracao_numeros <- 1
@@ -49,9 +45,13 @@ vetor_numeros_para_ordenacao <-
   sample(
     numero_inicial_para_geracao_numeros:numero_final_para_geracao_numeros, 
     qtd_elementos, 
-    replace=TRUE
+    replace=FALSE
   )
 
 vetor_numeros_para_ordenacao
-bubble_sort_gadelha(vetor_numeros_para_ordenacao)
-bubble_sort_gadelha(vetor_numeros_para_ordenacao, "DESC")
+mgg_bubble_sort(vetor_numeros_para_ordenacao)
+mgg_bubble_sort(vetor_numeros_para_ordenacao, "DESC")
+
+vetor_numeros_para_ordenacao
+mgg_bubble_sort_2(vetor_numeros_para_ordenacao)
+mgg_bubble_sort_2(vetor_numeros_para_ordenacao, "DESC")
